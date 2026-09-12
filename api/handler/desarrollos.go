7384f070aeca8a70d8b133aec7f78495d66c7ef6
Ismael-es <80365369+Ismael-es@users.noreply.github.com>
2026-09-12 15:15:42 -0300

    - se arreglo el quilombo sorry

--- api/handler/desarrollos.go
+++ api/handler/desarrollos.go
@@ -203,3 +203,20 @@ func (h *DesarrolloHandler) Borrar(c *gin.Context) {
 	}
 	c.Redirect(http.StatusSeeOther, "/admin/desarrollos")
 }
+
+// ViewPublica renderiza la vista pública de desarrollos en Desarrollos.html
+func (h *DesarrolloHandler) ViewPublica(c *gin.Context) {
+	desarrollos, err := h.service.GetAll()
+	if err != nil {
+		h.logger.Error("Error al obtener desarrollos para vista pública Desarrollos.html", zap.Error(err))
+		desarrollos = []desarrollo.Desarrollo{}
+	}
+
+	_, loggedIn := CurrentUserID(c)
+
+	c.HTML(http.StatusOK, "Desarrollos.html", gin.H{
+		"Desarrollos": desarrollos,
+		"LoggedIn":    loggedIn,
+	})
+}
+
