10bc0dfedfbe11cd90765921a4e4780007d2270f
RibZu <109860001+RibZu@users.noreply.github.com>
2026-09-12 16:16:29 -0300

    Delete ViewPublica method from DesarrolloHandler
    Removed the ViewPublica method from DesarrolloHandler.

--- api/handler/desarrollos.go
+++ api/handler/desarrollos.go
@@ -220,17 +220,3 @@ func (h *DesarrolloHandler) ViewPublica(c *gin.Context) {
 	})
 }
 
-func (h *DesarrolloHandler) ViewPublica(c *gin.Context) {
-	lista, err := h.service.GetAll()
-	if err != nil {
-		h.logger.Error("Error al obtener desarrollos para la página pública", zap.Error(err))
-		lista = []desarrollo.Desarrollo{}
-	}
-
-	_, loggedIn := CurrentUserID(c)
-
-	c.HTML(http.StatusOK, "Desarrollos.html", gin.H{
-		"Desarrollos": lista,
-		"LoggedIn":    loggedIn,
-	})
-}
